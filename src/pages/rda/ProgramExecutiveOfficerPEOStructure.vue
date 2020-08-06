55422e549b1c76edc1f421224d9d940c13df5074
Trey <phlasc@gmail.com>
2020-08-06 14:15:21 -0400

    add slide

---
+++ src/pages/rda/ProgramExecutiveOfficerPEOStructure.vue
@@ -0,0 +1,52 @@
+<template>
+  <Card>
+    <h1 slot="Title">Program Executive Officer (PEO) Structure</h1>
+    <div slot="MainContent">
+      <div class="FundConceptWrapper">
+        <figure>
+          <img :src="Image" alt="Graphic flow chart">
+        </figure>
+      </div>
+    </div>
+  </Card>
+</template>
+<style scoped>
+
+.FundConceptWrapper {
+  width: 100%;
+  height: auto;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+
+.FundConceptWrapper > figure {
+  max-width: calc(1426px / 2);
+
+}
+
+.FundConceptWrapper > figure > img {
+  width: 80%;
+  max-width: calc(1426px / 2);
+  height: auto;
+}
+
+</style>
+<script>
+import Card from '../../components/Card.vue';
+
+export default {
+
+  components: {
+    Card
+  },
+  name: 'ProgramExecutiveOfficerPEOStructure',
+  
+  data() {
+    return {
+      Image: require('@/assets/Program Executive Officer (PEO) Structure.png')
+    }
+  }
+  
+}
+</script>
