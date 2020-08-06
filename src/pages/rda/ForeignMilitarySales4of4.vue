66cc1abc5b37a8b114b3cbd6ecd5a5d1c504878b
Trey <phlasc@gmail.com>
2020-08-06 14:14:59 -0400

    add slide

---
+++ src/pages/rda/ForeignMilitarySales4of4.vue
@@ -0,0 +1,54 @@
+<template>
+  <Card>
+    <h1 slot="Title">Foreign Military Sales (4 of 4)</h1>
+    <div slot="MainContent">
+      <div class="FundConceptWrapper">
+        <figure>
+          <img :src="Image" alt="Graphic flow chart">
+        </figure>
+      </div>
+    </div>
+  </Card>
+</template>
+<style scoped>
+.ollist > li {
+  margin-bottom: 0;
+}
+
+.FundConceptWrapper {
+  width: 100%;
+  height: auto;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+
+.FundConceptWrapper > figure {
+  max-width: calc(1467px / 2);
+
+}
+
+.FundConceptWrapper > figure > img {
+  width: 80%;
+  max-width: calc(1467px / 2);
+  height: auto;
+}
+</style>
+<script>
+import Card from '../../components/Card.vue';
+
+export default {
+
+  components: {
+    Card
+  },
+  name: 'ForeignMilitarySales4of4',
+  
+  data() {
+    return {
+      Image: require('@/assets/Foreign_Military_Sales_(4 of 4.png')
+    }
+  }
+  
+}
+</script>
