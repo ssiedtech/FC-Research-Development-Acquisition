594470401fbd391e96d1f5aad656665978e79519
Trey <phlasc@gmail.com>
2020-08-06 14:13:56 -0400

    add slide

---
+++ src/pages/rda/ArmyResearchandDevelopmentObjective.vue
@@ -0,0 +1,37 @@
+<template>
+  <Card>
+    <h1 slot="Title">Army Research and Development Objective</h1>
+    <div slot="MainContent">
+      <ul>
+        <li>
+          Develop superior weapons, equipment and technology to those of ANY potential enemy, in any environment, and under all conditions of war.
+        </li>
+        <li>
+          Bottom Line: Equip the warfighter to win.
+        </li>
+      </ul>
+    </div>
+  </Card>
+</template>
+<style scoped>
+
+
+</style>
+<script>
+import Card from '../../components/Card.vue';
+
+export default {
+
+  components: {
+    Card
+  },
+  name: 'TestSlide',
+  
+  data() {
+    return {
+      Image: ''
+    }
+  }
+  
+}
+</script>
