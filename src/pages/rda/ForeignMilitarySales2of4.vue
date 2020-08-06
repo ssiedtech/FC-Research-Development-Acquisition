af91fef6cf13b4434e88a8e3f410126d9d38fa53
Trey <phlasc@gmail.com>
2020-08-06 14:14:26 -0400

    add slide

---
+++ src/pages/rda/ForeignMilitarySales2of4.vue
@@ -0,0 +1,38 @@
+<template>
+  <Card>
+    <h1 slot="Title">Foreign Military Sales (2 of 4)</h1>
+    <div slot="MainContent">
+      <p><b>Letter of Request (LOR)</b></p>
+      <ol class="ollist">
+        <li>Country Requests made through the State Department.</li>
+        <li>Congress Notified of Sale.</li>
+        <li>Army Subject Matter Experts advise political arm on appropriate capability.</li>
+      </ol>
+    </div>
+  </Card>
+</template>
+<style scoped>
+.ollist > li {
+  margin-bottom: 0;
+}
+
+
+</style>
+<script>
+import Card from '../../components/Card.vue';
+
+export default {
+
+  components: {
+    Card
+  },
+  name: 'ForeignMilitarySales2of4',
+  
+  data() {
+    return {
+      Image: ''
+    }
+  }
+  
+}
+</script>
