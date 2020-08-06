9056328a1e8d0c435ccd8af56b304f11080563bd
Trey <phlasc@gmail.com>
2020-08-06 14:14:03 -0400

    add slide

---
+++ src/pages/rda/DefenseAcquisitionBoardDAB.vue
@@ -0,0 +1,34 @@
+<template>
+  <Card>
+    <h1 slot="Title">Defense Acquisition Board (DAB)</h1>
+    <div slot="MainContent">
+      <ul>
+        <li>Senior DoD forum on all acquisition matters</li>
+        <li>Chaired by the Defense Acquisition Executive (DAE) – USD (AT&L)</li>
+        <li>Reviews Major Defense Acquisition Programs (MDAPs)</li>
+      </ul>
+    </div>
+  </Card>
+</template>
+<style scoped>
+
+
+</style>
+<script>
+import Card from '../../components/Card.vue';
+
+export default {
+
+  components: {
+    Card
+  },
+  name: 'DefenseAcquisitionBoardDAB',
+  
+  data() {
+    return {
+      Image: ''
+    }
+  }
+  
+}
+</script>
