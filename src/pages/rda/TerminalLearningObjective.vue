ab3f0baa3041d12e27e127108d5986669dc3a31f
Trey <phlasc@gmail.com>
2020-08-06 14:15:46 -0400

    add slide

---
+++ src/pages/rda/TerminalLearningObjective.vue
@@ -0,0 +1,37 @@
+<template>
+  <Card>
+    <h1 slot="Title">Terminal Learning Objective</h1>
+    <div slot="MainContent">
+      <p><b>Action</b><br>
+      Identify the Research, Development, and Acquisition (RDA) System.</p>
+
+      <p><b>Condition</b><br>
+      Given a summary sheet containing DoD 5000.1, AR 1-1, AR 11-18, AR 5-22 and DA PAM 70-3. </p>
+
+      <p><b>Standard</b><br>
+      With 80% accuracy, determine research and development objective; verify history and background; recognize key players and review board; determine the acquisition process; and define acquisition resource management.</p>
+    </div>
+  </Card>
+</template>
+<style scoped>
+
+
+</style>
+<script>
+import Card from '../../components/Card.vue';
+
+export default {
+
+  components: {
+    Card
+  },
+  name: 'TerminalLearningObjective',
+  
+  data() {
+    return {
+      Image: ''
+    }
+  }
+  
+}
+</script>
