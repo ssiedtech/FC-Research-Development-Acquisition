d914d7cdf91f191a1f2d0d10cd967cfcb9c2da31
Trey <phlasc@gmail.com>
2020-08-06 14:11:17 -0400

    refactor card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,44 +1,13 @@
 <template>
-<section class="TabsWrapper">
-    <v-tabs
-        style="border-radius: 10px; background-color: transparent;"
-        class="TabsWrapper"
-        background-color="transparent"
-    >
-        <v-tab>Slide</v-tab>
-        <v-tab>Notes</v-tab>
-
-        <v-tab-item>
-            <v-container fluid>
-                <div class="InnerContent pa-5">
-                    <slot name="Title"></slot>
-                    <div class="Line"></div>
-                    <slot class="WrapperContentModifier" name="MainContent"></slot>
-                </div>
-            </v-container>
-        </v-tab-item>
-        <v-tab-item>
-            <v-container fluid>
-                <div class="InnerNotesContent pa-5">
-                    <slot name="Notes" />
-                </div>
-            </v-container>
-        </v-tab-item>
-
-    </v-tabs>
-    
-
-</section>
+    <section class="TabsWrapper">
+        <div class="InnerContent pa-5">
+            <slot name="Title"></slot>
+            <div class="Line"></div>
+            <slot name="MainContent"></slot>
+        </div>
+    </section>
 </template>
 <style>
-.TabsWrapper > div.theme--dark.v-tabs-items {
-    border-radius: 10px;
-}
-
-.TabsWrapper > div.v-item-group.theme--dark.v-slide-group > div.v-slide-group__wrapper {
-    margin-left: 10px;
-}
-
 p {
     font-size: 24px;
     line-height: 24px;
@@ -57,32 +26,58 @@ ul > li {
     margin-bottom: 32px;
 }
 
+ul > li > b {
+    font-size: 24px;
+    line-height: 30px;
+    font-weight: 700;
+    margin-bottom: 32px;
+}
+
+
+ol > li {
+    font-size: 24px;
+    line-height: 30px;
+    font-weight: 300;
+    margin-bottom: 32px;
+}
+
+ol > li > b {
+    font-size: 24px;
+    line-height: 30px;
+    font-weight: 700;
+    margin-bottom: 32px;
+}
+
+
+
 table > tr > td, table > tr > th {
     font-size: 24px;
     line-height: 30px;
     font-weight: 300;
     margin-bottom: 32px;
 }
-</style>
-<style scoped>
-    .InnerContent > h1 {
-        font-size: 30px;
-        line-height: 30px;
-        font-weight: 400;
-        color: red;
-    }
 
-    .InnerContent > .Line {
-        border-bottom: 3px solid white;
-        width: 100px;
-        margin-top: 32px;
-        margin-bottom: 42px;
-    }
-    
-    .InnerContent {
-        width: 100%;
-    }
-    
+.InnerContent > h1 {
+    font-size: 30px;
+    line-height: 30px;
+    font-weight: 600;
+}
+
+.InnerContent > .Line {
+    border-bottom: 3px solid white;
+    width: 100px;
+    margin-top: 32px;
+    margin-bottom: 42px;
+}
+
+.InnerContent {
+    width: 100%;
+}
+.TabsWrapper {
+    background-color: #1e1e1e;
+    border-radius: 10px;
+    padding: 10px;
+}
 </style>
 
 <script>
