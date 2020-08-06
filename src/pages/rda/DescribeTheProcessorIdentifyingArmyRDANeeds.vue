3ef66b9f5bfdb26eca6aadce465e85a0cda9b6d8
Trey <phlasc@gmail.com>
2020-08-06 14:14:12 -0400

    add slide

---
+++ src/pages/rda/DescribeTheProcessorIdentifyingArmyRDANeeds.vue
@@ -0,0 +1,34 @@
+<template>
+  <Card>
+    <h1 slot="Title">Describe The Process or Identifying Army RDA Needs</h1>
+    <div slot="MainContent">
+      <ul>
+        <li>
+          How does the Army determine when to develop new weapon systems?
+        </li>
+      </ul>
+    </div>
+  </Card>
+</template>
+<style scoped>
+
+
+</style>
+<script>
+import Card from '../../components/Card.vue';
+
+export default {
+
+  components: {
+    Card
+  },
+  name: 'DescribeTheProcessorIdentifyingArmyRDANeeds',
+  
+  data() {
+    return {
+      Image: ''
+    }
+  }
+  
+}
+</script>
