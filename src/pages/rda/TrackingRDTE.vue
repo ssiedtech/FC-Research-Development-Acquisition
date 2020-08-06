ed29908de7feaa271bc4081a437acb2146f73ad7
Trey <phlasc@gmail.com>
2020-08-06 14:16:08 -0400

    add slide

---
+++ src/pages/rda/TrackingRDTE.vue
@@ -0,0 +1,53 @@
+<template>
+  <Card>
+    <h1 slot="Title">Tracking RDTE</h1>
+    <div slot="MainContent">
+      <p><b>How to track and control RDTE Funding </b></p>
+      <div class="FundConceptWrapper">
+        <figure>
+          <img :src="Image" alt="Graphic flow chart">
+        </figure>
+      </div>
+    </div>
+  </Card>
+</template>
+<style scoped>
+
+.FundConceptWrapper {
+  width: 100%;
+  height: auto;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+
+.FundConceptWrapper > figure {
+  max-width: calc(1825px / 2);
+
+}
+
+.FundConceptWrapper > figure > img {
+  width: 80%;
+  max-width: calc(1825px / 2);
+  height: auto;
+}
+
+</style>
+<script>
+import Card from '../../components/Card.vue';
+
+export default {
+
+  components: {
+    Card
+  },
+  name: 'Tracking RDTE',
+  
+  data() {
+    return {
+      Image: require('@/assets/Tracking_RDT&E.png')
+    }
+  }
+  
+}
+</script>
