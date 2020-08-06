275b98cf9838f9e507c8bb9baaffb9cd0a9aac18
Trey <phlasc@gmail.com>
2020-08-06 14:15:39 -0400

    add slide

---
+++ src/pages/rda/RequirementsDetermination.vue
@@ -0,0 +1,52 @@
+<template>
+  <Card>
+    <h1 slot="Title">Requirements Determination</h1>
+    <div slot="MainContent">
+      <div class="FundConceptWrapper">
+        <figure>
+          <img :src="Image" alt="Graphic flow chart">
+        </figure>
+      </div>
+    </div>
+  </Card>
+</template>
+<style scoped>
+
+.FundConceptWrapper {
+  width: 100%;
+  height: auto;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+
+.FundConceptWrapper > figure {
+  max-width: calc(1219px / 2);
+
+}
+
+.FundConceptWrapper > figure > img {
+  width: 80%;
+  max-width: calc(1219px / 2);
+  height: auto;
+}
+
+</style>
+<script>
+import Card from '../../components/Card.vue';
+
+export default {
+
+  components: {
+    Card
+  },
+  name: 'RequirementsDetermination',
+  
+  data() {
+    return {
+      Image: require('@/assets/Requirements_Determination.png')
+    }
+  }
+  
+}
+</script>
